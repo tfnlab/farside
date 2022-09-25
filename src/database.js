dd081df5e32ed802ee45a635c416fa3de82f7a1b
tfnLAB <tfnlab@gmail.com>
2022-09-25 07:58:56 -0700

    test

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ module.exports = {
   "0": {
     "glburl": "https://majpo.com/download.aw.glb.jsp?nft=0",
     "imgurl": "https://md.majpo.com/images/0.png",
-    "tribe": "qoHvam Duj lengmeH SuvwI'pu'",
+    "tribe": "QoHvam Duj lengmeH SuvwI'pu'",
     "rank": "Sa'"
   },
   "1": {
@@ -10,5 +10,11 @@ module.exports = {
     "imgurl": "https://md.majpo.com/images/1.png",
     "tribe": "Hivje'ngan",
     "rank": "Du'"
+  },
+  "2": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=2",
+    "imgurl": "https://md.majpo.com/images/2.png",
+    "tribe": "TlhIv",
+    "rank": "Mang'"
   }
 }
