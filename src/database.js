b919c3f6c58f26f5b0331ff7c286e55c59260d6f
tfnLAB <tfnlab@gmail.com>
2022-09-25 10:08:23 -0700

    test

--- src/database.js
+++ src/database.js
@@ -358,5 +358,365 @@ module.exports = {
     "imgurl": "https://md.majpo.com/images/5.png",
     "tribe": "TlhIv",
     "rank": "ruv"
-  }  
+  },
+  "60": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=0",
+    "imgurl": "https://md.majpo.com/images/0.png",
+    "tribe": "QoHvam Duj lengmeH SuvwI'pu'",
+    "rank": "Sa'"
+  },
+  "61": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=1",
+    "imgurl": "https://md.majpo.com/images/1.png",
+    "tribe": "Hivje'ngan",
+    "rank": "Du'"
+  },
+  "62": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=2",
+    "imgurl": "https://md.majpo.com/images/2.png",
+    "tribe": "TlhIv",
+    "rank": "Mang'"
+  },
+  "63": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=3",
+    "imgurl": "https://md.majpo.com/images/3.png",
+    "tribe": "TlhIv",
+    "rank": "'e' luQIjbe'lu'chugh'"
+  },
+  "64": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=4",
+    "imgurl": "https://md.majpo.com/images/4.png",
+    "tribe": "TlhIv",
+    "rank": "Qeb"
+  },
+  "65": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=5",
+    "imgurl": "https://md.majpo.com/images/5.png",
+    "tribe": "TlhIv",
+    "rank": "ruv"
+  },
+  "66": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=0",
+    "imgurl": "https://md.majpo.com/images/0.png",
+    "tribe": "QoHvam Duj lengmeH SuvwI'pu'",
+    "rank": "Sa'"
+  },
+  "67": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=1",
+    "imgurl": "https://md.majpo.com/images/1.png",
+    "tribe": "Hivje'ngan",
+    "rank": "Du'"
+  },
+  "68": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=2",
+    "imgurl": "https://md.majpo.com/images/2.png",
+    "tribe": "TlhIv",
+    "rank": "Mang'"
+  },
+  "69": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=3",
+    "imgurl": "https://md.majpo.com/images/3.png",
+    "tribe": "TlhIv",
+    "rank": "'e' luQIjbe'lu'chugh'"
+  },
+  "70": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=4",
+    "imgurl": "https://md.majpo.com/images/4.png",
+    "tribe": "TlhIv",
+    "rank": "Qeb"
+  },
+  "71": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=5",
+    "imgurl": "https://md.majpo.com/images/5.png",
+    "tribe": "TlhIv",
+    "rank": "ruv"
+  },
+  "72": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=6",
+    "imgurl": "https://md.majpo.com/images/6.png",
+    "tribe": "QeH",
+    "rank": "Sa'"
+  },
+  "73": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=0",
+    "imgurl": "https://md.majpo.com/images/0.png",
+    "tribe": "QoHvam Duj lengmeH SuvwI'pu'",
+    "rank": "Sa'"
+  },
+  "74": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=1",
+    "imgurl": "https://md.majpo.com/images/1.png",
+    "tribe": "Hivje'ngan",
+    "rank": "Du'"
+  },
+  "75": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=2",
+    "imgurl": "https://md.majpo.com/images/2.png",
+    "tribe": "TlhIv",
+    "rank": "Mang'"
+  },
+  "76": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=3",
+    "imgurl": "https://md.majpo.com/images/3.png",
+    "tribe": "TlhIv",
+    "rank": "'e' luQIjbe'lu'chugh'"
+  },
+  "77": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=4",
+    "imgurl": "https://md.majpo.com/images/4.png",
+    "tribe": "TlhIv",
+    "rank": "Qeb"
+  },
+  "78": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=5",
+    "imgurl": "https://md.majpo.com/images/5.png",
+    "tribe": "TlhIv",
+    "rank": "ruv"
+  },
+  "79": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=0",
+    "imgurl": "https://md.majpo.com/images/0.png",
+    "tribe": "QoHvam Duj lengmeH SuvwI'pu'",
+    "rank": "Sa'"
+  },
+  "20": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=1",
+    "imgurl": "https://md.majpo.com/images/1.png",
+    "tribe": "Hivje'ngan",
+    "rank": "Du'"
+  },
+  "81": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=2",
+    "imgurl": "https://md.majpo.com/images/2.png",
+    "tribe": "TlhIv",
+    "rank": "Mang'"
+  },
+  "82": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=3",
+    "imgurl": "https://md.majpo.com/images/3.png",
+    "tribe": "TlhIv",
+    "rank": "'e' luQIjbe'lu'chugh'"
+  },
+  "83": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=4",
+    "imgurl": "https://md.majpo.com/images/4.png",
+    "tribe": "TlhIv",
+    "rank": "Qeb"
+  },
+  "84": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=5",
+    "imgurl": "https://md.majpo.com/images/5.png",
+    "tribe": "TlhIv",
+    "rank": "ruv"
+  },
+  "85": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=6",
+    "imgurl": "https://md.majpo.com/images/6.png",
+    "tribe": "QeH",
+    "rank": "Sa'"
+  },
+  "86": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=2",
+    "imgurl": "https://md.majpo.com/images/2.png",
+    "tribe": "TlhIv",
+    "rank": "Mang'"
+  },
+  "87": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=3",
+    "imgurl": "https://md.majpo.com/images/3.png",
+    "tribe": "TlhIv",
+    "rank": "'e' luQIjbe'lu'chugh'"
+  },
+  "88": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=4",
+    "imgurl": "https://md.majpo.com/images/4.png",
+    "tribe": "TlhIv",
+    "rank": "Qeb"
+  },
+  "89": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=5",
+    "imgurl": "https://md.majpo.com/images/5.png",
+    "tribe": "TlhIv",
+    "rank": "ruv"
+  },
+  "90": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=0",
+    "imgurl": "https://md.majpo.com/images/0.png",
+    "tribe": "QoHvam Duj lengmeH SuvwI'pu'",
+    "rank": "Sa'"
+  },
+  "91": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=1",
+    "imgurl": "https://md.majpo.com/images/1.png",
+    "tribe": "Hivje'ngan",
+    "rank": "Du'"
+  },
+  "92": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=2",
+    "imgurl": "https://md.majpo.com/images/2.png",
+    "tribe": "TlhIv",
+    "rank": "Mang'"
+  },
+  "93": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=3",
+    "imgurl": "https://md.majpo.com/images/3.png",
+    "tribe": "TlhIv",
+    "rank": "'e' luQIjbe'lu'chugh'"
+  },
+  "94": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=4",
+    "imgurl": "https://md.majpo.com/images/4.png",
+    "tribe": "TlhIv",
+    "rank": "Qeb"
+  },
+  "95": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=5",
+    "imgurl": "https://md.majpo.com/images/5.png",
+    "tribe": "TlhIv",
+    "rank": "ruv"
+  },
+  "96": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=0",
+    "imgurl": "https://md.majpo.com/images/0.png",
+    "tribe": "QoHvam Duj lengmeH SuvwI'pu'",
+    "rank": "Sa'"
+  },
+  "97": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=1",
+    "imgurl": "https://md.majpo.com/images/1.png",
+    "tribe": "Hivje'ngan",
+    "rank": "Du'"
+  },
+  "98": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=2",
+    "imgurl": "https://md.majpo.com/images/2.png",
+    "tribe": "TlhIv",
+    "rank": "Mang'"
+  },
+  "99": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=3",
+    "imgurl": "https://md.majpo.com/images/3.png",
+    "tribe": "TlhIv",
+    "rank": "'e' luQIjbe'lu'chugh'"
+  },
+  "100": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=4",
+    "imgurl": "https://md.majpo.com/images/4.png",
+    "tribe": "TlhIv",
+    "rank": "Qeb"
+  },
+  "101": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=5",
+    "imgurl": "https://md.majpo.com/images/5.png",
+    "tribe": "TlhIv",
+    "rank": "ruv"
+  },
+  "102": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=6",
+    "imgurl": "https://md.majpo.com/images/6.png",
+    "tribe": "QeH",
+    "rank": "Sa'"
+  },
+  "103": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=0",
+    "imgurl": "https://md.majpo.com/images/0.png",
+    "tribe": "QoHvam Duj lengmeH SuvwI'pu'",
+    "rank": "Sa'"
+  },
+  "104": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=1",
+    "imgurl": "https://md.majpo.com/images/1.png",
+    "tribe": "Hivje'ngan",
+    "rank": "Du'"
+  },
+  "105": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=2",
+    "imgurl": "https://md.majpo.com/images/2.png",
+    "tribe": "TlhIv",
+    "rank": "Mang'"
+  },
+  "106": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=3",
+    "imgurl": "https://md.majpo.com/images/3.png",
+    "tribe": "TlhIv",
+    "rank": "'e' luQIjbe'lu'chugh'"
+  },
+  "107": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=4",
+    "imgurl": "https://md.majpo.com/images/4.png",
+    "tribe": "TlhIv",
+    "rank": "Qeb"
+  },
+  "108": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=5",
+    "imgurl": "https://md.majpo.com/images/5.png",
+    "tribe": "TlhIv",
+    "rank": "ruv"
+  },
+  "109": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=0",
+    "imgurl": "https://md.majpo.com/images/0.png",
+    "tribe": "QoHvam Duj lengmeH SuvwI'pu'",
+    "rank": "Sa'"
+  },
+  "110": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=1",
+    "imgurl": "https://md.majpo.com/images/1.png",
+    "tribe": "Hivje'ngan",
+    "rank": "Du'"
+  },
+  "111": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=2",
+    "imgurl": "https://md.majpo.com/images/2.png",
+    "tribe": "TlhIv",
+    "rank": "Mang'"
+  },
+  "112": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=3",
+    "imgurl": "https://md.majpo.com/images/3.png",
+    "tribe": "TlhIv",
+    "rank": "'e' luQIjbe'lu'chugh'"
+  },
+  "113": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=4",
+    "imgurl": "https://md.majpo.com/images/4.png",
+    "tribe": "TlhIv",
+    "rank": "Qeb"
+  },
+  "114": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=5",
+    "imgurl": "https://md.majpo.com/images/5.png",
+    "tribe": "TlhIv",
+    "rank": "ruv"
+  },
+  "115": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=6",
+    "imgurl": "https://md.majpo.com/images/6.png",
+    "tribe": "QeH",
+    "rank": "Sa'"
+  },
+  "116": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=2",
+    "imgurl": "https://md.majpo.com/images/2.png",
+    "tribe": "TlhIv",
+    "rank": "Mang'"
+  },
+  "117": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=3",
+    "imgurl": "https://md.majpo.com/images/3.png",
+    "tribe": "TlhIv",
+    "rank": "'e' luQIjbe'lu'chugh'"
+  },
+  "118": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=4",
+    "imgurl": "https://md.majpo.com/images/4.png",
+    "tribe": "TlhIv",
+    "rank": "Qeb"
+  },
+  "119": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=5",
+    "imgurl": "https://md.majpo.com/images/5.png",
+    "tribe": "TlhIv",
+    "rank": "ruv"
+  }
 }
