cee9bb886c665b432866f28aef668d237185ed8a
tfnLAB <tfnlab@gmail.com>
2022-09-25 07:20:27 -0700

    test

--- src/database.js
+++ src/database.js
@@ -4,5 +4,11 @@ module.exports = {
     "imgurl": "https://md.majpo.com/images/0.png",
     "tribe": "qoHvam Duj lengmeH SuvwI'pu'",
     "rank": "Sa'"
+  },
+  "0": {
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=0",
+    "imgurl": "https://md.majpo.com/images/0.png",
+    "tribe": "Hivje'ngan",
+    "rank": "Du'"
   }
 }
