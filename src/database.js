bc2d413b5fc4d68be5fef8f1b71d4374b03956ea
tfnLAB <tfnlab@gmail.com>
2022-09-25 07:29:04 -0700

    test

--- src/database.js
+++ src/database.js
@@ -6,8 +6,8 @@ module.exports = {
     "rank": "Sa'"
   },
   "0": {
-    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=0",
-    "imgurl": "https://md.majpo.com/images/0.png",
+    "glburl": "https://majpo.com/download.aw.glb.jsp?nft=1",
+    "imgurl": "https://md.majpo.com/images/1.png",
     "tribe": "Hivje'ngan",
     "rank": "Du'"
   }
